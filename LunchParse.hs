41921835611e6b812019cd51a9eebb1f3be10495
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-01-07 22:23:30 +0100

    NF: Simple hack for a lunch menu parsing application.

---
+++ LunchParse.hs
@@ -0,0 +1,57 @@
+
+import System.IO
+import System.Environment
+import System.Console.GetOpt
+import System.Exit
+
+import Data.Functor
+import qualified Data.Text as T
+
+import Text.XML.HXT.Core
+import Text.XML.HXT.Curl
+
+mensaURL = "http://www.studentenwerk-berlin.de/speiseplan/rss/hu_nord/tag/lang/0000000000000000000000000"
+
+main :: IO ()
+main = do
+    menu <- runX $ application [ withCurl [] ] mensaURL
+    mapM_ maybePrint menu
+  where
+    maybePrint str =
+      let stripped = T.strip $ T.pack str
+      in if (not $ T.null stripped) then putStrLn $ T.unpack stripped
+         else return ()
+    --if rc >= c_err
+    --  then exitWith (ExitFailure (0-1))
+    --  else exitWith ExitSuccess
+
+
+application     :: SysConfigList -> String -> IOSArrow b String
+application cfg src
+    = configSysVars cfg                                           -- (0)
+      >>>
+      readDocument [] src
+      >>>
+      processDocumentRootElement
+      >>>
+--      writeDocumentToString [withOutputPLAIN ] -- dst                                        -- (3)
+--      >>>
+      readFromString [withParseHTML yes]
+      >>>
+      (deep ( isElem >>> hasClass "mensa_day_speise_name" >>> getChildren >>> getText))
+      -- >>>
+      -- writeDocumentToString [withOutputPLAIN ] -- dst                                        -- (3)
+--      >>>
+--      getErrStatus
+
+hasClass cls = hasAttrValue "class" chk
+  where
+    chk s = cls `elem` (words s)
+
+processDocumentRootElement      :: IOSArrow XmlTree String
+processDocumentRootElement      = (deep ( isElem >>> hasName "description" >>> getChildren >>> getText ))
+--      >>> withTraceLevel 4 (traceValue 1 show)
+ 
+
+--    return ()
+
