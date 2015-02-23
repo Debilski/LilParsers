62b9f870b5e2740451845eefc804522305786b8e
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-02-23 13:36:27 +0100

    NF: Print a book

---
+++ BookPrint.hs
@@ -0,0 +1,36 @@
+import Prelude hiding (putStr)
+import Data.ByteString.Char8 (putStr)
+import Data.ByteString.UTF8 (fromString)
+
+import System.IO hiding (putStr)
+import System.Directory
+import System.FilePath.Find
+import Data.Random
+import Data.Random.Lift
+import Data.Random.Source.DevRandom
+import Data.Random.Extras
+
+import Data.List
+import Control.Monad
+import Control.Monad.IO.Class
+
+choiceWindow :: Int -> [a] -> RVar [a]
+choiceWindow _ [] = return []
+choiceWindow size xs = do
+  count <- uniform 0 (max (length xs - size) 0)
+  return $ take size (drop count xs)
+
+
+getSnip :: [FilePath] -> Int -> RVarT IO [Char]
+getSnip files size = do
+    file <- lift $ choice files
+    chars <- liftIO $ readFile file
+    snip <- lift $ choiceWindow size chars
+    return snip
+
+main = do
+    files <- getCurrentDirectory >>= getDirectoryContents >>= filterM (return . isSuffixOf ".txt")
+    snip <- runRVarT (getSnip files 500) DevURandom
+    putStr $ (fromString (snip ++ "\n"))
+    hFlush stdout
+
